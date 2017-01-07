ae2de485a3a15e6b8be823fd96bdd30b56ac0058
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-01-06 21:15:30 -0500

    Generate more pages and styling

--- static/js/diary/script.js
+++ static/js/diary/script.js
@@ -1,5 +1,17 @@
-$("#flipbook").turn({
-		width: 400,
-		height: 300,
-		autoCenter: true
-	});
+generatePages(20, function() {
+    $("#flipbook").turn({
+        width: 1000,
+        height: 800,
+        autoCenter: true
+    });
+})
+
+
+
+function generatePages(numPages, callback) {
+    console.log("Generate pages");
+    for (var i = 0; i < numPages; i++) {
+        $("#flipbook").append('<div class="hard">' + i + '</div>');
+    }
+    callback();
+}
