2c9210cc9f6704c1e0274f4451061caec0fa2a37
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-02-20 07:50:07 -0500

    need to get hex from hover

--- static/js/butterfly/script.js
+++ static/js/butterfly/script.js
@@ -27,17 +27,38 @@ $("canvas").click(function(e) {
     //requestAnimationFrame(spawnBlackness);
 })
 
+//get pixel of where mouse is
+$('canvas').mousemove(function(e) {
+    var pos = findPos(this);
+    var x = e.pageX - pos.x;
+    var y = e.pageY - pos.y;
+    var coord = "x=" + x + ", y=" + y;
+    var c = this.getContext('2d');
+    var p = c.getImageData(x, y, 1, 1).data; 
+    var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+    
+    console.log(coord + "<br>" + hex);
+});
 
-/* //REcursive exampe
-// Animate.
-function animate(highResTimestamp) {
-  requestAnimationFrame(animate);
-  // Animate something...
+function findPos(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
 }
 
-// Start the animation.
-requestAnimationFrame(animate);
- */
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
