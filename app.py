8dbbb317d69e321b8b5b086978ab29190dd5b6d9
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-02-24 10:58:25 -0500

    Make generating works dynamic

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import render_template
 
 app = Flask(__name__)
 
+my_works = [{'url':'castleinblood','name':'Castle in Blood','credit':["Lucy","Stephanie"],'date':'12/31/16'},
+{'url':'butterfly','name':'Butterfly','credit':["Lucy","Kaijie"],'date':''},
+{'url':'chapel_entrance','name':'Chapel in Light','credit':["Lucy", "Yuhao Hu"],'date':'2/22/17'},
+{'url':'deathnote','name':'Death Note','credit':['Lucy'],'date':'1/9/17'}]
 
 @app.route("/")
 @app.route("/home")
@@ -12,15 +16,22 @@ def home():
 
 @app.route("/works")
 def works():
-	return render_template('works.html')
+	print works
+	return render_template('works.html', works = my_works)
 
 @app.route("/about")
 def about():
 	return render_template('about.html')
 
+@app.route('/works/<variable>', methods=['GET'])
+def daily_post(variable):
+    #do your code here
+    return render_template(variable+"/index.html")
+
+
 @app.route("/castleinblood")
 def castle():
-	return render_template('castleinblood/test.html')
+	return render_template('castleinblood/index.html')
 
 @app.route("/butterfly")
 def butterfly():
