86cc5d4f74e5d4019917377850ba471af74a2ec2
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-02-24 21:11:16 -0500

    Added butterfly

--- static/js/butterfly/script.js
+++ static/js/butterfly/script.js
@@ -1,9 +1,10 @@
 var canvas = $("canvas");
 
 var spread = 200;
-var w = 10
+var w = 5
 var h = w
 var alphaDiff = 0.0005
+var maxOpacity = 200;
 
 var ctx = canvas[0].getContext("2d"),
     width = $(document).width(),
@@ -31,7 +32,14 @@ $("canvas").click(function(e) {
     //requestAnimationFrame(spawnBlackness);
 })
 
+function getOpacity(x, y) {
+    var p = ctx.getImageData(x, y, 1, 1).data;
+    var opac = p[3]; //opacity, 255 if black
+    return parseInt(opac);
+}
+
 //get pixel of where mouse is
+/*
 $('canvas').mousemove(function(e) {
     var pos = findPos(this);
     var x = e.pageX - pos.x;
@@ -45,9 +53,61 @@ $('canvas').mousemove(function(e) {
     
     console.log(coord + "<br>" + hex);
 });
+*/
+
+function okToMove(opacity) {
+    return (opacity < maxOpacity);
+}
+
+function getNewPosition() {
+    //get random position
+    var width = $("#background").width();
+    var height = $("#background").height();
+
+    var new_y = Math.floor(Math.random()*height);
+    var new_x = Math.floor(Math.random()*width);
+
+    var old_x = $("#butterfly").offset().left;
+    var old_y = $("#butterfly").offset().top;
+    return [new_x-old_x, new_y-old_y]
+
+}
+
+function butterflyMove(opacity=null) {
+    var new_pos = getNewPosition();
+    var speed = 5//calcSpeed([old_pos.top, old_pos.left], new_pos);
+    console.log(new_pos)
+    $("#butterfly_container").animate({
+        top: new_pos[1],//new_pos[0],
+        left: new_pos[0]//new_pos[1]
+    }, speed, function(){
+        butterflyMove();
+    })
+
+}
+
+$(document).ready(function(){
+    butterflyMove();
+})
+
+function calcSpeed(prev, next) {
+
+    var x = Math.abs(prev[1] - next[1]);
+    var y = Math.abs(prev[0] - next[0]);
+
+    var greatest = x > y ? x : y;
+
+    var speedModifier = 0.1;
+
+    var speed = Math.ceil(greatest / speedModifier);
+
+    return speed;
+
+}
 
 function findPos(obj) {
-    var curleft = 0, curtop = 0;
+    var curleft = 0,
+        curtop = 0;
     if (obj.offsetParent) {
         do {
             curleft += obj.offsetLeft;
@@ -66,26 +126,26 @@ function rgbToHex(r, g, b) {
 
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function spawnBlackness(x, y, width, height, alpha, i) {
     console.log("width: " + width + "; height: " + height)
-    var rand = getRandomInt(-100,100);
+    var rand = getRandomInt(-100, 100);
     alpha += alphaDiff;
-    parseInt(Math.random() * 2) ?  coordRand1 =1 : coordRand1 = -1;
-    parseInt(Math.random() * 2) ?  coordRand2 =1 : coordRand2 = -1;
-    var scale = getRandomInt(Math.pow(i, 1/3),Math.pow(i,1/3)+2);
-    x+=coordRand1*scale;
-    y+=coordRand2*scale;
+    parseInt(Math.random() * 2) ? coordRand1 = 1 : coordRand1 = -1;
+    parseInt(Math.random() * 2) ? coordRand2 = 1 : coordRand2 = -1;
+    var scale = getRandomInt(Math.pow(i, 1 / 3), Math.pow(i, 1 / 3) + 2);
+    x += coordRand1 * scale;
+    y += coordRand2 * scale;
     i++;
-    console.log("i: "+i)
+    console.log("i: " + i)
     ctx.fillRect(x, y, width, height);
     ctx.fillStyle = "rgba(0,0,0," + alpha + ")";
     requestID = requestAnimationFrame(function() {
-        spawnBlackness(x, y, width, height, alpha,i);
+        spawnBlackness(x, y, width, height, alpha, i);
 
     });
     if (i > spread) {
