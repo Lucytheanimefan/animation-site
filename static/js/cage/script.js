91db0542f599e57c7d3f92bb5e2090bd6c5a57d0
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-06-18 19:49:57 -0400

    Set mouse hover to erase canvas for cage project

--- static/js/cage/script.js
+++ static/js/cage/script.js
@@ -9,6 +9,7 @@ const cageCorner = canvas.width / cubeDepth;
 
 $(document).ready(function() {
   generateBars();
+  setMouseHover();
 })
 
 function generateBars() {
@@ -17,23 +18,23 @@ function generateBars() {
   //left coming in
   for (var i = 0; i < 24; i++) {
     var leftcoords = generateCoordinatesFromLinearEqn(0.5, i * 30, i, cageCorner);
-    animateLines("left" + i, ctx, leftcoords, 1, "white", 1, 0);
+    animateLines("left" + i, ctx, leftcoords, 1, "grey", 1, 0);
 
     var rightcoords = generateCoordinatesFromLinearEqn(-0.5, i * 30 + cageCorner, cageCorner, canvas.width);
     if (i == 0) {
       firstLeftCoords = leftcoords;
       firstRightCoords = rightcoords;
     }
-    animateLines("right" + i, ctx, rightcoords, 1, "white", 1, 0);
+    animateLines("right" + i, ctx, rightcoords, 1, "DarkGrey", 1, 0);
 
   }
 
   for (let k = 0; k < 14; k++) {
     // The top
-    let topcoords = generateCoordinatesFromLinearEqn(-0.5, k * 30, k + 29*k, canvas.width);
+    let topcoords = generateCoordinatesFromLinearEqn(-0.5, k * 30, k + 29 * k, canvas.width);
 
     //console.log(topcoords)
-    animateLines("top" + k, ctx, topcoords, 1, "blue", 1, 0);
+    animateLines("top" + k, ctx, topcoords, 1, "white", 1, 0);
   }
   //vertical bars
   for (var j = 0; j < 70; j++) {
@@ -50,4 +51,16 @@ function generateCoordinatesFromLinearEqn(slope, xintercept, start, end) {
   }
   //console.log(coords[0]);
   return coords;
+}
+
+function setMouseHover() {
+  canvas.onmousemove = function(e) {
+    // important: correct mouse position:
+    var rect = this.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top,
+      i = 0,
+      r;
+    ctx.clearRect(x, y, cubeDepth, cubeDepth); // for demo
+  }
 }
