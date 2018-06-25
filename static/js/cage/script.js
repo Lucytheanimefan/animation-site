82e8a3e4591f02e63280061c0a088749cfec558a
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-06-24 21:43:45 -0400

    Get rid of spirograph from cage

--- static/js/cage/script.js
+++ static/js/cage/script.js
@@ -82,36 +82,6 @@ function setMouseHover() {
       i = 0,
       r;
 
-    // Check for background spirograph
-    // Normalize
-    let nX = x - canvas.width / 2;
-    let nY = y - canvas.height / 2;
-    let rad = Math.atan2(nY, nX); // radians
-
-    // Draw the spirograph
-    x_t = R * ((1 - k) * Math.cos(rad) + l * k * Math.cos(((1 - k) * rad) / k));
-    y_t = R * ((1 - k) * Math.sin(rad) + l * k * Math.sin(((1 - k) * rad) / k));
-
-    // Unnormalize
-    x_t += canvas.width/2;
-    y_t += canvas.height/2;
-
-    let drawSpirograph = function(x, y) {
-      //let diff = Math.abs(x - y);
-      if (count % 9 == 0) {
-        ctx.fillStyle = "#F7BDBC";
-      } else if (count % 8 == 0) {
-        ctx.fillStyle = "#B2F2D3";
-      } else if (count % 7 == 0) {
-        ctx.fillStyle = "#B3E6D6";
-      } else if (count % 6 == 0) {
-        ctx.fillStyle = "#B4BCB5";
-      }
-      ctx.globalAlpha = 0.1;
-      ctx.fillRect(x, y, backgroundImageClearArea, backgroundImageClearArea);
-      ctx.globalAlpha = 1;
-    }
-
     // let diff = Math.abs(x - y);
     // if (diff < 100) {
     // if (diff < 20){
@@ -133,7 +103,6 @@ function setMouseHover() {
     ctx.clearRect(x, y, clearArea, clearArea);
     ctx.clearRect(y, x, clearArea, clearArea);
 
-    drawSpirograph(x_t, y_t);
 
     // Create an "X"
     let xinterceptpos = getXIntercept(1, x, y);
