59a5f3e9030b433a90ff375d956087170315f3d8
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-06-25 22:14:21 -0400

    Luminescence based on distance from center

--- static/js/spirograph/script.js
+++ static/js/spirograph/script.js
@@ -6,14 +6,16 @@ const ctx = canvas.getContext('2d');
 
 var count = 0;
 
-const squareArea = 40;
+const squareArea = 20;
 
-const clearArea = squareArea/2;
+const clearArea = squareArea / 2;
+
+const diagonalDistance = Math.sqrt(Math.pow(Math.abs(canvas.width / 2), 2) + Math.pow(Math.abs(canvas.height / 2), 2));
 
 // Spirograph
 // The parameter 0<=l<=1 represents how far the point A is located from the center of Ci. At the same time, 0<=k<=1 represents how big the inner circle Ci is with respect to the outer one Co.
 const k = 0.1;
-const l = 0.9;
+const l = 0.99;
 const R = 300;
 
 $(document).ready(function() {
@@ -38,23 +40,29 @@ function setMouseHover() {
     let rad = Math.atan2(nY, nX); // radians
 
     // Draw the spirograph
-    x_t = R * ((1 - k) * Math.cos(rad) + l * k * Math.cos(((1 - k) * rad) / k));
-    y_t = R * ((1 - k) * Math.sin(rad) + l * k * Math.sin(((1 - k) * rad) / k));
+    x_t = R * ((1 - k) * Math.cos(rad) + (l * k) * Math.cos(((1 - k) * rad) / k));
+    y_t = R * ((1 - k) * Math.sin(rad) + (l * k) * Math.sin(((1 - k) * rad) / k));
 
     // Unnormalize
-    x_t += canvas.width/2;
-    y_t += canvas.height/2;
+    x_t += canvas.width / 2;
+    y_t += canvas.height / 2;
 
+    var distFactor = Math.sqrt(Math.pow(Math.abs(x - canvas.width / 2), 2) + Math.pow(Math.abs(y - canvas.height / 2), 2)) / diagonalDistance;
+
+    if (distFactor < 0.5) {
+      distFactor *= -1;
+    }
+
+    //console.log("distFactor: " + distFactor);
     let drawSpirograph = function(x, y) {
-      //let diff = Math.abs(x - y);
       if (count % 9 == 0) {
-        ctx.fillStyle = "#F7BDBC";
+        ctx.fillStyle = ColorLuminance("#F7BDBC", distFactor);
       } else if (count % 8 == 0) {
-        ctx.fillStyle = "#B2F2D3";
+        ctx.fillStyle = ColorLuminance("#B2F2D3", distFactor);
       } else if (count % 7 == 0) {
-        ctx.fillStyle = "#B3E6D6";
+        ctx.fillStyle = ColorLuminance("#B3E6D6", distFactor);
       } else if (count % 6 == 0) {
-        ctx.fillStyle = "#B4BCB5";
+        ctx.fillStyle = ColorLuminance("#B4BCB5", distFactor);
       }
       ctx.globalAlpha = 0.1;
       ctx.fillRect(x, y, squareArea, squareArea);
@@ -84,4 +92,25 @@ function setMouseHover() {
 
     drawSpirograph(x_t, y_t);
   }
+}
+
+function ColorLuminance(hex, lum) {
+
+  // validate hex string
+  hex = String(hex).replace(/[^0-9a-f]/gi, '');
+  if (hex.length < 6) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  lum = lum || 0;
+
+  // convert to decimal and change luminosity
+  var rgb = "#",
+    c, i;
+  for (i = 0; i < 3; i++) {
+    c = parseInt(hex.substr(i * 2, 2), 16);
+    c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+    rgb += ("00" + c).substr(c.length);
+  }
+  //console.log(rgb);
+  return rgb;
 }
