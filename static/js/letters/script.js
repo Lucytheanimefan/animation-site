46dcbcfb3317518f271c96b73f442257a60e4fc7
Lucy Zhang <spothorse9.lucy@gmail.com>
2019-01-13 15:52:28 -0800

    WIP letters

---
+++ static/js/letters/script.js
@@ -0,0 +1,11 @@
+// 1) Set the canvas size
+var canvas = $("main");
+
+var ctx = canvas[0].getContext("2d"),
+    width = $(document).width(),
+    height = $(document).height(),
+    a, b;
+
+canvas.attr("width", width);
+canvas.attr("height", height);
+
